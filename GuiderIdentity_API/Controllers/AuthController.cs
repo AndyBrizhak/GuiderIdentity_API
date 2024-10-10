337de39b3419b50bf2567321a4f12ff845f37e74
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-10-10 17:37:56 +0800

    add controller authentication

---
+++ GuiderIdentity_API/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using GuiderIdentity_API.Data;
+using GuiderIdentity_API.Migrations;
+using GuiderIdentity_API.Models;
+using GuiderIdentity_API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuiderIdentity_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+        public AuthController(ApplicationDbContext db, 
+            IConfiguration configuration,
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>
+                ("ApiSettings:Secret"); 
+            _response = new ApiResponse();
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>
+            Register([FromBody] RegisterRequestDTO model)
+        {
+
+        }
+
+    }
+}
