7cec4e9b9f6ff68d6e71d7b829a6a5dab9a3e675
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-10-10 19:39:15 +0800

    registration users

--- GuiderIdentity_API/Controllers/AuthController.cs
+++ GuiderIdentity_API/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using GuiderIdentity_API.Migrations;
 using GuiderIdentity_API.Models;
 using GuiderIdentity_API.Models.DTO;
+using GuiderIdentity_API.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -30,11 +31,70 @@ namespace GuiderIdentity_API.Controllers
             _roleManager = roleManager;
         }
 
-        [HttpPost]
-        public async Task<IActionResult>
-            Register([FromBody] RegisterRequestDTO model)
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
         {
+            ApplicationUser userFromDb = 
+                _db.ApplicationUsers
+                .FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
 
+            if (userFromDb != null)
+            {
+                _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username already exist");
+                return BadRequest(_response);
+            }
+
+            ApplicationUser newUser = new()
+            {
+                UserName = model.UserName,
+                Email = model.UserName,
+                NormalizedUserName = model.UserName.ToUpper(),
+                Name = model.Name
+            };
+
+            try
+            {
+                var result =
+                await _userManager.CreateAsync(newUser, model.Password);
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync(Utility.SD.Role_Admin)
+                        .GetAwaiter().GetResult())
+                    {
+                        //create role on database
+                        await _roleManager
+                            .CreateAsync(new IdentityRole(SD.Role_Admin));
+                        await _roleManager
+                            .CreateAsync(new IdentityRole(SD.Role_Customer));
+                    }
+
+                    if (model.Role.ToLower() == SD.Role_Admin)
+                    {
+                        await _userManager
+                            .AddToRoleAsync(newUser, SD.Role_Admin);
+                    }
+                    else
+                    {
+                        await _userManager
+                            .AddToRoleAsync(newUser, SD.Role_Customer);
+                    }
+                    _response.StatusCode = System.Net.HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            
+            _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Error while rgistration");
+            return BadRequest(_response);
         }
 
     }
--- GuiderIdentity_API/Program.cs
+++ GuiderIdentity_API/Program.cs
@@ -13,7 +13,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+//simple registration
+builder.Services.Configure<IdentityOptions>(options=>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+});
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
